5350be7f8c2b98a5398a61cde5e79f64e65df39e
Kory <kalbert1991@gmail.com>
2023-01-08 18:03:14 -0800

    Update search.php

--- search.php
+++ search.php
@@ -11,9 +11,15 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get the user-provided location criteria
-$city = $_POST["city"];
-$state = $_POST["state"];
+// Get the user-provided location
+$location = $_POST["location"];
+
+// Split the location on the comma character
+$parts = explode(",", $location);
+
+// Trim any leading/trailing white space from the parts
+$city = trim($parts[0]);
+$state = trim($parts[1]);
 
 // Construct the SELECT statement
 $sql = "SELECT * FROM people WHERE city='$city' AND state='$state'";
@@ -30,42 +36,4 @@ if (!$result) {
 $people = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 // Free the result set
-mysqli_free_result($result);
-
-// Close the connection
-mysqli_close($conn);
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Search Results</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <h1>Search Results</h1>
-
-<?php
-// Check if any people were found
-if (count($people) > 0) {
-  // Display the results in an HTML table
-  echo "<table>";
-  echo "<tr><th>Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th></tr>";
-  foreach ($people as $person) {
-    echo "<tr>";
-    echo "<td>" . $person["name"] . "</td>";
-    echo "<td>" . $person["address"] . "</td>";
-    echo "<td>" . $person["city"] . "</td>";
-    echo "<td>" . $person["state"] . "</td>";
-    echo "<td>" . $person["zip_code"] . "</td>";
-    echo "</tr>";
-  }
-  echo "</table>";
-} else {
-  // No people were found
-  echo "<p>No people were found for the specified location.</p>";
-}
-?>
-
-</body>
-</html>
+mysqli_free
